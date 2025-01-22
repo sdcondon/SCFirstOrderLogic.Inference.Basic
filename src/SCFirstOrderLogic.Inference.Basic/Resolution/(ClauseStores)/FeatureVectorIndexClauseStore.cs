c0910eb35de8f424baff1d0ad097fb66dd0d70ca
Simon Condon <simoncondon@hotmail.com>
2025-01-22 19:54:09 +0000

    Added FeatureVectorIndexClauseStore for resolution KB.

---
+++ src/SCFirstOrderLogic.Inference.Basic/Resolution/(ClauseStores)/FeatureVectorIndexClauseStore.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) 2021-2024 Simon Condon.
+// You may use this file in accordance with the terms of the MIT license.
+using SCFirstOrderLogic.ClauseIndexing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SCFirstOrderLogic.Inference.Basic.Resolution;
+
+/// <summary>
+/// An implementation of <see cref="IKnowledgeBaseClauseStore"/> that maintains all known clauses in
+/// a feature vector index.
+/// </summary>
+public class FeatureVectorIndexClauseStore<TNode, TFeature> : IKnowledgeBaseClauseStore
+    where TNode : IAsyncFeatureVectorIndexNode<TFeature, CNFClause>, ICloneable, IDisposable
+    where TFeature : notnull
+{
+    private readonly Func<CNFClause, IEnumerable<FeatureVectorComponent<TFeature>>> featureVectorSelector;
+    private readonly TNode featureVectorIndexRoot;
+    private readonly AsyncFeatureVectorIndex<TFeature> featureVectorIndex;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeatureVectorIndexClauseStore{TNode, TFeature}"/> class.
+    /// </summary>
+    public FeatureVectorIndexClauseStore(
+        Func<CNFClause, IEnumerable<FeatureVectorComponent<TFeature>>> featureVectorSelector,
+        TNode featureVectorIndexRoot)
+    {
+        this.featureVectorSelector = featureVectorSelector;
+        this.featureVectorIndexRoot = featureVectorIndexRoot;
+
+        featureVectorIndex = new AsyncFeatureVectorIndex<TFeature>(
+            featureVectorSelector,
+            featureVectorIndexRoot);
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> AddAsync(CNFClause clause, CancellationToken cancellationToken = default)
+    {
+        return await featureVectorIndex.TryReplaceSubsumedAsync(clause);
+    }
+
+    /// <inheritdoc />
+    public IAsyncEnumerator<CNFClause> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    {
+        return featureVectorIndex.GetAsyncEnumerator(cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public Task<IQueryClauseStore> CreateQueryStoreAsync(CancellationToken cancellationToken = default)
+    {
+        // todo: clone -> something async?
+        return Task.FromResult<IQueryClauseStore>(new QueryStore(
+            featureVectorSelector,
+            (TNode)featureVectorIndexRoot.Clone()));
+    }
+
+    /// <summary>
+    /// Implementation of <see cref="IQueryClauseStore"/> that is used solely by <see cref="HashSetClauseStore"/>.
+    /// </summary>
+    private class QueryStore : IQueryClauseStore
+    {
+        private readonly TNode featureVectorIndexRoot;
+        private readonly AsyncFeatureVectorIndex<TFeature> featureVectorIndex;
+
+        public QueryStore(
+            Func<CNFClause, IEnumerable<FeatureVectorComponent<TFeature>>> featureVectorSelector,
+            TNode featureVectorIndexRoot)
+        {
+            this.featureVectorIndexRoot = featureVectorIndexRoot;
+
+            featureVectorIndex = new AsyncFeatureVectorIndex<TFeature>(
+                featureVectorSelector,
+                featureVectorIndexRoot);
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> AddAsync(CNFClause clause, CancellationToken cancellationToken = default)
+        {
+            return await featureVectorIndex.TryReplaceSubsumedAsync(clause);
+        }
+
+        /// <inheritdoc />
+        public IAsyncEnumerator<CNFClause> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            return featureVectorIndex.GetAsyncEnumerator(cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public async IAsyncEnumerable<ClauseResolution> FindResolutions(
+            CNFClause clause,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            await foreach (var otherClause in this.WithCancellation(cancellationToken))
+            {
+                foreach (var resolution in ClauseResolution.Resolve(clause, otherClause))
+                {
+                    yield return resolution;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            featureVectorIndexRoot.Dispose();
+        }
+    }
+}
